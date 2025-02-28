e2f05d971f0809967e98baf2ecef0a2e785ffff8
PaulinoFelipe <154370191+oFelipeee@users.noreply.github.com>
2025-02-28 09:47:15 -0300

    .
    exercicios feito

---
+++ emprestimo.py
@@ -0,0 +1,12 @@
+print("------------------------------------------------------")
+renda = float(input("Digite o valor da sua renda mensal: "))
+parcela = float(input("Digite o valor da parcela desejada: "))
+print("------------------------------------------------------")
+
+
+if renda > 2000 and parcela <= 0.3 * renda:
+    print("Empréstimo aprovado!  :)     ")
+elif renda <= 2000:
+    print("Empréstimo negado: renda insuficiente. :(     ")
+else:
+    print("Empréstimo negado: parcela acima de 30% da renda. :(    ")
---
+++ paridade_num.py
@@ -0,0 +1,9 @@
+print("----------------------------------------")
+numero = int(input("Digite um número inteiro: "))
+print("----------------------------------------")
+
+
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
